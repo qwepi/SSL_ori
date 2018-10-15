34514779cc74641311da59e782794c883a153b05
Jing Chen <jingchen@cerc.utexas.edu>
2018-10-15 11:22:02 -0500

    modify test_SSL.py

--- test_SSL.py
+++ test_SSL.py
@@ -1,9 +1,8 @@
-from model import *
+from model_SSL import *
 import ConfigParser as cp
 import sys
 import time
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = str(sys.argv[2])
 from progress.bar import Bar
 
 '''
@@ -18,7 +17,6 @@ test_path   = infile.get('dir','test_path')
 model_path = infile.get('dir','model_path')
 fealen     = int(infile.get('feature','ft_length'))
 blockdim   = int(infile.get('feature','block_dim'))
-aug=int(infile.get('feature','AUG'))
 
 '''
 Prepare the Input
@@ -27,18 +25,8 @@ test_data = data(test_path, test_path+'/label.csv')
 x_data = tf.placeholder(tf.float32, shape=[None, blockdim*blockdim, fealen])              #input FT
 y_gt   = tf.placeholder(tf.float32, shape=[None, 2])                                      #ground truth label
 x      = tf.reshape(x_data, [-1, blockdim, blockdim, fealen])                             #reshap to NHWC
-x_ud   = tf.map_fn(lambda img: tf.image.flip_up_down(img), x)                   #up down flipped
-x_lr   = tf.map_fn(lambda img: tf.image.flip_left_right(img), x)                #left right flipped
-x_lu   = tf.map_fn(lambda img: tf.image.flip_up_down(img), x_lr)                #both flipped
-predict_or = forward(x, is_training=False)                                      #do forward
-predict_ud = forward(x_ud, is_training=False, reuse=True)  
-predict_lr = forward(x_lr, is_training=False, reuse=True)
-predict_lu = forward(x_lu, is_training=False, reuse=True)
-if aug==1:
-    predict = (predict_or + predict_lr + predict_lu + predict_ud)/4.0
-else:
-    predict = predict_or
-#predict = predict_or
+predict, predict1 = forward_crosstask(x, is_training = False)
+
 y      = tf.cast(tf.argmax(predict, 1), tf.int32)                                         
 accu   = tf.equal(y, tf.cast(tf.argmax(y_gt, 1), tf.int32))                               #calc batch accu
 accu   = tf.reduce_mean(tf.cast(accu, tf.float32))
